7f7f2b7dcc177c6f27efb5cda059546499285aa8
PiterNaGitHub <filipdziwani306@gmail.com>
2024-08-02 19:12:20 +0200

    Update fly.lua

--- fly.lua
+++ fly.lua
@@ -1,7 +1,7 @@
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;
 Notify({
-Description = "removedxd on discord";
+Description = "simple cframe fly script removedxd on discord for report the bugs";
 Title = "made by piter";
 Duration = 15;
 });
@@ -11,83 +11,83 @@ getgenv().FakeMacro = true
 	sound.SoundId = "rbxassetid://413861777"
 	sound.Parent = game:GetService("SoundService")
 	sound:Play()
-repeat wait() 
-    until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
-local mouse = game.Players.LocalPlayer:GetMouse() 
-repeat wait() until mouse
-local plr = game.Players.LocalPlayer 
-local torso = plr.Character.Head 
-local flying = false
-local deb = true 
-local ctrl = {f = 0, b = 0, l = 0, r = 0} 
-local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-local maxspeed = 5000 ------- change ur max fly speed here :D
-local speed = 500 -------- change ur speed here :D
+local Player = game:GetService("Players").LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local StarterGui = game:GetService("StarterGui")
+local Lighting = game:GetService("Lighting")
+local Camera = game:GetService("Workspace").CurrentCamera
  
-function Fly() 
-local bg = Instance.new("BodyGyro", torso) 
-bg.P = 9e4 
-bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
-bg.cframe = torso.CFrame 
-local bv = Instance.new("BodyVelocity", torso) 
-bv.velocity = Vector3.new(0,0.1,0) 
-bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
-repeat wait() 
-plr.Character.Humanoid.PlatformStand = true 
-if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
-speed = speed+.5+(speed/maxspeed) 
-if speed > maxspeed then 
-speed = maxspeed 
-end 
-elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
-speed = speed-1 
-if speed < 0 then 
-speed = 0 
-end 
-end 
-if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
-bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
-elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
-bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-else 
-bv.velocity = Vector3.new(0,0.1,0) 
-end 
-bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
-until not flying 
-ctrl = {f = 0, b = 0, l = 0, r = 0} 
-lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-speed = 0 
-bg:Destroy() 
-bv:Destroy() 
-plr.Character.Humanoid.PlatformStand = false 
-end 
-mouse.KeyDown:connect(function(key) 
-if key:lower() == "v" then ------ change ur keybind here :D
-if flying then flying = false 
-else 
-flying = true 
-Fly() 
-end 
-elseif key:lower() == "w" then 
-ctrl.f = 1 
-elseif key:lower() == "s" then 
-ctrl.b = -1 
-elseif key:lower() == "a" then 
-ctrl.l = -1 
-elseif key:lower() == "d" then 
-ctrl.r = 1 
-end 
-end) 
-mouse.KeyUp:connect(function(key) 
-if key:lower() == "w" then 
-ctrl.f = 0 
-elseif key:lower() == "s" then 
-ctrl.b = 0 
-elseif key:lower() == "a" then 
-ctrl.l = 0 
-elseif key:lower() == "d" then 
-ctrl.r = 0 
-end 
+local Fly = false
+local FlyPart = nil
+local FlySpeed = 75
+ 
+local Keys = {W = false, A = false, S = false, D = false}
+ 
+if game.Lighting:FindFirstChild("TheEvilDeveloper:Fl1") then
+    return nil
+end
+UserInputService.InputBegan:Connect(function(Key)
+    if Key.KeyCode == Enum.KeyCode.Z then
+        Fly = not Fly
+    elseif Key.KeyCode == Enum.KeyCode.W then
+        Keys.W = true
+    elseif Key.KeyCode == Enum.KeyCode.A then
+        Keys.A = true
+    elseif Key.KeyCode == Enum.KeyCode.S then
+        Keys.S = true
+    elseif Key.KeyCode == Enum.KeyCode.D then
+        Keys.D = true
+    end
 end)
-Fly()
+ 
+UserInputService.InputEnded:Connect(function(Input)
+    if Input.KeyCode == Enum.KeyCode.W then
+        Keys.W = false
+    elseif Input.KeyCode == Enum.KeyCode.A then
+        Keys.A = false
+    elseif Input.KeyCode == Enum.KeyCode.S then
+        Keys.S = false
+    elseif Input.KeyCode == Enum.KeyCode.D then
+        Keys.D = false
+    end
+end)
+ 
+RunService.Heartbeat:Connect(function()
+    local RootPart = Player.Character.HumanoidRootPart
+    if FlyPart == nil then 
+        FlyPart = Instance.new("Part", Lighting)
+        FlyPart.Name = "TheEvilDeveloper:Fl1"
+        FlyPart.Size = RootPart.Size
+    end
+    if Fly ~= false then
+        RootPart.CFrame = FlyPart.CFrame
+    else
+        FlyPart.CFrame = RootPart.CFrame
+    end
+end)
+ 
+while true do
+    local Delta = RunService.RenderStepped:Wait()
+    local RootPart = Player.Character.HumanoidRootPart
+    local Humanoid = Player.Character:FindFirstChildOfClass("Humanoid")
+    if Humanoid == nil then
+        return
+    end
+    Camera:GetPropertyChangedSignal("CFrame"):Connect(function()
+        if Fly then
+            RootPart.CFrame = CFrame.new(RootPart.CFrame.Position, RootPart.CFrame.Position + Camera.CFrame.LookVector)
+        end
+    end)
+    if Fly then
+        if Keys.W then
+            FlyPart.CFrame = RootPart.CFrame + (Camera.CFrame.LookVector * (Delta * FlySpeed))
+        elseif Keys.A then
+            FlyPart.CFrame = RootPart.CFrame + (-Camera.CFrame.RightVector * (Delta * FlySpeed))
+        elseif Keys.S then
+            FlyPart.CFrame = RootPart.CFrame + (-Camera.CFrame.LookVector * (Delta * FlySpeed))
+        elseif Keys.D then
+            FlyPart.CFrame = RootPart.CFrame + (Camera.CFrame.RightVector * (Delta * FlySpeed))
+        end
+    end
+end
