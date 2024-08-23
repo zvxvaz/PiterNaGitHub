aec25ec6863f60b56f115052eb78a3e08c9b6a43
PiterNaGitHub <filipdziwani306@gmail.com>
2024-08-23 19:19:09 +0200

    Update macro.lua

--- macro.lua
+++ macro.lua
@@ -1,9 +1,9 @@
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/PiterNaGitHub/PiterNaGitHub/main/text.lua"))();
 local Notify = AkaliNotif.Notify;
 Notify({
-Description = "macro script has been executed success press x to macro join up our discord server: https://discord.gg/k93hppJwEm";
-Title = "made by piter";
-Duration = 20;
+Description = "shutdown.";
+Title = "bye.";
+Duration = 15;
 });
 getgenv().Speed = true
 getgenv().FakeMacro = true
@@ -11,19 +11,3 @@ getgenv().FakeMacro = true
 	sound.SoundId = "rbxassetid://413861777"
 	sound.Parent = game:GetService("SoundService")
 	sound:Play()
-local Player = game:GetService("Players").LocalPlayer
-            local Mouse = Player:GetMouse()
-            local SpeedGlitch = false
-            Mouse.KeyDown:Connect(function(Key)
-                if Key == "x" then
-                    SpeedGlitch = not SpeedGlitch
-                    if SpeedGlitch == true then
-                        repeat game:GetService("RunService").Heartbeat:wait()
-                               game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", true, game)
-                                    game:GetService("RunService").Heartbeat:wait()
-                                    game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", false, game)
-                                    game:GetService("RunService").Heartbeat:wait()
-                        until SpeedGlitch == false
-                    end
-                end
-            end)
