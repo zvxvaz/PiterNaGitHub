556d6934d64da17a2aa51532b2eb6aea6acbf4fa
zvxvaz <filipdziwani306@gmail.com>
2025-02-07 19:09:17 +0100

    Create text1.lua

---
+++ text1.lua
@@ -0,0 +1,13 @@
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+Notify({
+Description = "";
+Title = "coded by @11404_";
+Duration = 15;
+});
+getgenv().Speed = true
+getgenv().FakeMacro = true
+	local sound = Instance.new("Sound")
+	sound.SoundId = "rbxassetid://413861777"
+	sound.Parent = game:GetService("SoundService")
+	sound:Play()
